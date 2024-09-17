cd73bb69a5d99625974bf3c9a97e8d1df79cf715
matthew-gigl-db <matthew-gigl-db>
2024-09-17 04:00:32 +0000

    genAI get struct schema ;-)

--- src/01-using_files.py
+++ src/01-using_files.py
@@ -140,5 +140,85 @@ display(df)
 
 # COMMAND ----------
 
+# MAGIC %sql
+# MAGIC describe select * from read_files(
+# MAGIC   "/Volumes/mgiglia/synthea/landing/patients/2024_09_14T19_14_42Z_patients.csv"
+# MAGIC   ,format => 'csv'
+# MAGIC   ,delimiter => ','
+# MAGIC   ,header => true
+# MAGIC )
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from read_files(
+# MAGIC   "/Volumes/mgiglia/synthea/landing/patients/2024_09_14T19_14_42Z_patients.csv"
+# MAGIC   ,format => 'csv'
+# MAGIC   ,delimiter => ','
+# MAGIC   ,header => true
+# MAGIC   ,schemaHints => 'FIPS string, ZIP string'
+# MAGIC )
+
+# COMMAND ----------
+
 # MAGIC %md
+# MAGIC ## Use the Databricks Assistant to Get the Struct Version of the Schema for pySpark
+# MAGIC ***
 # MAGIC
+# MAGIC Note that the last dataframe value from our SQL statement is stored as _sqldf.  
+# MAGIC
+# MAGIC Type "command i" to open the Databricks Assistant and type:  "return the struct schema of _sqldf"
+
+# COMMAND ----------
+
+
+from pyspark.sql.types import *
+
+patients_schema = StructType([
+    StructField("Id", StringType(), True),
+    StructField("BIRTHDATE", DateType(), True),
+    StructField("DEATHDATE", DateType(), True),
+    StructField("SSN", StringType(), True),
+    StructField("DRIVERS", StringType(), True),
+    StructField("PASSPORT", StringType(), True),
+    StructField("PREFIX", StringType(), True),
+    StructField("FIRST", StringType(), True),
+    StructField("MIDDLE", StringType(), True),
+    StructField("LAST", StringType(), True),
+    StructField("SUFFIX", StringType(), True),
+    StructField("MAIDEN", StringType(), True),
+    StructField("MARITAL", StringType(), True),
+    StructField("RACE", StringType(), True),
+    StructField("ETHNICITY", StringType(), True),
+    StructField("GENDER", StringType(), True),
+    StructField("BIRTHPLACE", StringType(), True),
+    StructField("ADDRESS", StringType(), True),
+    StructField("CITY", StringType(), True),
+    StructField("STATE", StringType(), True),
+    StructField("COUNTY", StringType(), True),
+    StructField("FIPS", IntegerType(), True),
+    StructField("ZIP", IntegerType(), True),
+    StructField("LAT", DoubleType(), True),
+    StructField("LON", DoubleType(), True),
+    StructField("HEALTHCARE_EXPENSES", DoubleType(), True),
+    StructField("HEALTHCARE_COVERAGE", DoubleType(), True),
+    StructField("INCOME", IntegerType(), True),
+    StructField("_rescued_data", StringType(), True)
+])
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Query All of the Files In the Directory With the Proper Schema
+# MAGIC ***
+
+# COMMAND ----------
+
+df = spark.read.format("csv") \
+    .option("header", "true") \
+    .option("delimiter", ",") \
+    .option("escape", '"') \
+    .schema(patients_schema) \
+    .load(f"/Volumes/{catalog_use}/synthea/landing/patients/")
+
+display(df)
